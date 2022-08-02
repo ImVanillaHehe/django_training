24d541bde10865ea914d791867ae60000c418e19
Vanilla <kadin0_0@icloud.com>
2022-08-02 21:49:11 +0300

    third commit

--- firstweb/views.py
+++ firstweb/views.py
@@ -7,7 +7,7 @@ from .forms import OrderForm
 def first_page(request):
     object_list = Order.objects.all()
     form = OrderForm()
-    return render(request, './index.html', {'object_list': object_list, 'form':form})
+    return render(request, './index.html', {'object_list': object_list, 'form': form})
 
 
 def thanks_page(request):
